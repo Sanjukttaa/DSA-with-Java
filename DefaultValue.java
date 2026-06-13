43f3517e51f1bc81dac55bd1576b72612bec2681
Sanjukta <Sanjuktaabiswas@gmail.com>
2026-06-13 21:30:54 +0530

    DefaultValue

---
+++ DefaultValue.java
@@ -0,0 +1,23 @@
+public class DefaultValue {
+   //Integer
+   static byte b;  // 8 bit or 1 byte
+   static short s;
+   static int i;
+   static long l;
+   static float f;
+   static double d;
+   static char ch;
+   static boolean bl;
+   static String str;
+  public static void main(String[] args) {
+   System.out.println("Default value of byte :"+b);
+   System.out.println("Default value of short :"+s);
+   System.out.println("Default value of int :"+i);
+   System.out.println("Default value of long :"+l);
+   System.out.println("Default value of float :"+f);
+   System.out.println("Default value of double :"+d);
+   System.out.println("Default value of char :"+ch);
+   System.out.println("Default value of boolean :"+bl);
+   System.out.println("Default value of String :"+str);
+  }
+}
--- primitivedt.java
+++
@@ -1,9 +0,0 @@
-public class DefaultValue {
-   //Integer
-   static byte b;  // 8 bit or 1 byte
-   static short s;
-  public static void main(String[] args) {
-   System.out.println("Default value of byte :"+b);
-   System.out.println("Default value of short :"+s);
-  }
-}
