711e344b62b94d25cd783ee3546a57f60d568234
Sanjukta <Sanjuktaabiswas@gmail.com>
2026-06-13 21:43:27 +0530

    ConstructorInJava

---
+++ ConstructorInJava.java
@@ -0,0 +1,20 @@
+public class ConstructorInJava{
+    int a, t;
+    ConstructorInJava(){  // default
+        System.out.println("It is default contructor..!");
+    }
+    ConstructorInJava(int a, int b){ // paramer
+        this.a=a;
+        t=b;
+    }
+    void show(){
+        System.out.println(a);
+        System.out.println(t);
+    }
+    public static void main(String[] args) {
+        ConstructorInJava c1=new ConstructorInJava();
+        ConstructorInJava c2=new ConstructorInJava(5, 8);
+        c1.show();
+        c2.show();
+    }
+}
